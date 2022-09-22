6da237f028a0c98d52d2f688f50a53110e40de9b
siqihuang1994 <113553582+siqihuang1994@users.noreply.github.com>
2022-09-22 13:40:50 -0400

    Update run.py

--- app/run.py
+++ app/run.py
@@ -9,6 +9,7 @@ from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
+from sklearn.base import BaseEstimator, TransformerMixin
 from sqlalchemy import create_engine
 
 
@@ -24,6 +25,25 @@ def tokenize(text):
         clean_tokens.append(clean_tok)
 
     return clean_tokens
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+    
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
